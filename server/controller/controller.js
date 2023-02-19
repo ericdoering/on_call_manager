8985041437f936b44fe33c6d2fe04b8b44d379e8
Eric Doering <ericbenjamindoering@gmail.com>
2023-02-18 21:51:29 -0500

    created database schema

---
+++ server/controller/controller.js
@@ -0,0 +1,22 @@
+var Userdb = require("../model/model");
+
+
+//Create and save new user
+exports.create = (req, res) => {
+
+}
+
+//Retrieve and return all users & Retrieve a single user
+exports.find = (req, res) => {
+
+}
+
+//Update a new user identified by their ID
+exports.update = (req, res) => {
+
+}
+
+//Delete a user with a specified user ID
+exports.delete = (req, res) => {
+
+}
