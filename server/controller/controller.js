4b1fe233dbdb1246c149c0dd9fee3101064d8c5b
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-12 17:18:14 -0400

    completion of create-user

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,7 +3,26 @@ var Userdb = require("../model/model");
 
 //Create and save new user
 exports.create = (req, res) => {
-
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty."});
+        return;
+    }
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status,
+    })
+    user
+        .save(user)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message : err.message || "An error occured while trying to create user."
+            })
+        })
 }
 
 //Retrieve and return all users & Retrieve a single user
