3f1d8799e07735efcbb21fecd1adb254177caf6e
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-25 20:46:09 -0400

    creating get request functionality

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,16 @@
+const axios = require("axios")
 
 exports.homeRoutes = (req, res) => {
-    res.render("index");
-}
+
+    axios.get(`http://localhost:3000/api/users`)
+    .then(function(response){
+        console.log(response.data)
+        res.render(`index`, { users : response.data })
+    })
+    .catch(err => {
+        res.send(err);
+    })
+};
 
 exports.add_user = (req, res) => {
     res.render("add_user")
